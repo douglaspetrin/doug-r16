fa6a6c3c14dc609810ef3d4022ae5e6746ace84e
Douglas Petrin <editor@douglaspetrin.com>
2018-11-10 20:08:38 -0200

    adding Aux tag instead of using div tag

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
 import classes from './Person.module.css';
 
 
-const person = (props) => {
+class Person extends Component {
+    render() {
+            return (
+                
+                <div className={classes.Person}>
+        
+                    <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}</p>
+                    <p>{this.props.children}</p>
+                    <input type="text" onChange={this.props.changed} placeholder={this.props.name}></input>
+        
+                </div>
+            )
+        }
+    }
 
-    return (
-        <div className={classes.Person}>
 
-            <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} placeholder={props.name}></input>
+export default Person;
 
-        </div>
-    )
-};
+// const person = (props) => {
 
-export default person;
+//     return (
+//         <div className={classes.Person}>
+
+//             <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
+//             <p>{props.children}</p>
+//             <input type="text" onChange={props.changed} placeholder={props.name}></input>
+
+//         </div>
+//     )
+// };
+
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const aux = (props) => props.children;
+
+export default aux;
+
+
+// HOC -> higher order component: they are not representational wrap other components to add certain functionality
